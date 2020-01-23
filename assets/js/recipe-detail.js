9b06f281e3d50d35731daa1be80da771a37df703
scott mikul <scott.p.mikul@gmail.com>
2020-01-23 13:37:24 -0500

    added a button that links to the restaurant page

--- assets/js/recipe-detail.js
+++ assets/js/recipe-detail.js
@@ -6,26 +6,14 @@ var urlParamsObj = getUrlParams(params);
 //assuming our href's in index.html are created with...
 // "/food-detail.html?food=" + selectedFood
 
-var recipeName;
 if (urlParamsObj["recipeid"]) {
-    recipeName = getFoodDetail(urlParamsObj["recipeid"]);
-    console.log(recipeName);
-} 
-
-
-
-//  recipe image: response[0].image 
-// title: response[0].label
-// button for restaurant search
-//  response[0].healthLabels[]
-//  response[0].ingredientLines[]
-//  response[0].calories
-//  preptime response[0].totalTime
-// url to original article: response[0].url
+    getFoodDetail(urlParamsObj["recipeid"]);
+}else{
+    getFoodDetail("recipeid=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_ce5a8f2f0ef46d8d83f8b13c731d3340");
+}
 
 //sample url url_to_this_page + recipeid=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_ce5a8f2f0ef46d8d83f8b13c731d3340
-
-// make a button that makes a reference to a restaurants page with lattitude, longitude, and the label
+//https://api.edamam.com/search?app_id=1d8d6cd0&app_key=378cb75e8a79027d393bfc99650de712&r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_ce5a8f2f0ef46d8d83f8b13c731d3340
 function getFoodDetail (recipeid) {
      //use foodName in a url for an ajax request to edamam (GET)
      var urlQuery = "https://api.edamam.com/search?app_id=1d8d6cd0&app_key=378cb75e8a79027d393bfc99650de712&r="+encodeURIComponent(recipeid)
@@ -33,19 +21,31 @@ function getFoodDetail (recipeid) {
          var newDiv = $("<div>");
          //image
          newDiv.append($("<img>").attr("src", response[0].image ));
+
+         //button to find restaurants
+         var restaurantSearch = $("<button>");
+         restaurantSearch.append("Search For Restaurant");
+         restaurantSearch.click(function(){
+            window.location.replace("restaurant.html?recipeName="+encodeURIComponent(response[0].label));
+         });
+         newDiv.append(restaurantSearch);
+
+
          //recipe title
-         newDiv.append($("<h2>").append(response[0].label));
+         newDiv.append($("<div>").append(response[0].label).addClass("title is-1"));
          healthLabelsSpan=  $("<span>");
          //make small?
+        //example tag :<span class="tag is-danger">Super danger</span>
         var healthLabels = response[0].healthLabels;
          healthLabels.forEach((label) => {
-            healthLabelsSpan.append(" "+label);
+            healthLabelsSpan.append("<span class=\"tag is-danger\">"+label+"</span>");
          })
          newDiv.append(healthLabelsSpan);
          ingredientLines = response[0].ingredientLines;
-         var ingredientLinesEL = $("<ul>")
+         var ingredientLinesEL = $("<div>").addClass("list");
          ingredientLines.forEach((line)=>{
-            var ingredientItem = $("<li>").append(line);
+            var ingredientItem = $("<div>").addClass("list-item");
+            ingredientItem.append(line);
             ingredientLinesEL.append(ingredientItem);
          });
          newDiv.append(ingredientLinesEL);
@@ -63,6 +63,9 @@ function getFoodDetail (recipeid) {
          originalArticleLink.append( response[0].url);
          originalArticle.append("Original Article: ").append(originalArticleLink);
          newDiv.append(originalArticle);
+
+
+
 
          $("#recipe-id").append(newDiv);
          console.log(response);
